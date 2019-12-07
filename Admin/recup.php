9c3c7ce6eff7329c58cfb08881b8af0c611e013a
Aurélien PUJOL <43996081+AurelPuj@users.noreply.github.com>
2019-12-07 11:30:48 +0100

    Update recup.php

--- Admin/recup.php
+++ Admin/recup.php
@@ -1,22 +1,20 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ecommerce";
-$connect = mysqli_connect($servername, $username, $password, $dbname);
-if($connect ==False){
-    echo 'probleme';
-}else{
-    $NomProduit=$_POST['NomProduit'];
-    $Url=$_POST['Url'];
-    $Description=$_POST['Description'];
-    $req= mysqli_query($connect, "INSERT INTO article (Ref,Nom,Image,Description) VALUES (0,'$NomProduit','$Url','$Description')");
-    if (!$req){
-        echo "prout";
-    }else{
-        header('Location: http://localhost/EcommercePhp/Admin/Liste.php'); 
-    }
+$Nom=$_POST['NomProduit'];
+$Image=$_POST['Url'];
+$Description=$_POST['Description'];
+$Quantité=$_POST['Quantité'];
+$Marque=$_POST['Marque'];
+$Catégorie=$_POST['Catégorie'];
+$Prix=$_POST['Prix'];
+$TVA=$_POST['TVA'];
 
+$connect = mysqli_connect('localhost', 'root', '', 'ecommerce');
 
+if (!$connect) {
+    echo "Echec de Connection : " . mysqli_connect_error();
 }
+
+$sql = 'INSERT INTO article VALUES("0","'.$Nom.'","'.$Image.'","'.$Description.'","'.$Quantité.'","'.$Marque.'","'.$Catégorie.'","'.$Prix.'","'.$TVA.'")';
+
+mysqli_query($connect, $sql) or die ('Erreur SQL !'.$sql.'<br />'. mysqli_error($connect));;
